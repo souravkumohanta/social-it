d7d07db690c2708de9ddaf9c7bfcecd406c83909
sourav ku mohanta <souravku457@gmail.com>
2020-08-11 10:45:38 +0530

    Application files

---
+++ Socialites/app/src/main/java/com/example/adityakumarsharma/socialites/github.java
@@ -0,0 +1,13 @@
+package com.example.adityakumarsharma.socialites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class github extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_github);
+    }
+}
